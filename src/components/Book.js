232bd6babd9f6e094575b9f9e4d633f2670f054c
denibulkashvili <denibulkashvili@gmail.com>
2019-03-01 15:00:40 +0700

    Style book cards

--- src/components/Book.js
+++ src/components/Book.js
@@ -8,19 +8,22 @@ class Book extends Component {
   get authors() {
     const authors = this.props.authors;
     if (authors === undefined) {
-      return "Unknown";
+      return "Author: Unknown";
     }
-    return authors.length === 1 ? authors[0] : authors.join(", ");
+    return authors.length === 1 ? `Author: ${authors[0]}` : `Authors: ${authors.join(", ")}` ;
   }
 
 
   render() {
     return (
       <div className="book-wrapper">
-        <img src={this.props.cover} alt="book-cover"/>
-        Title: {this.props.title} 
-        Authors: {this.authors}
-        Link: <a href={this.props.url}>See Inside</a>
+        <img className="book-cover" src={this.props.cover} alt="book-cover"/>
+        <div className="book-content">
+          <p className="book-title">Title: {this.props.title} </p>
+          <p className="book-authors">{this.authors} </p>
+          <p className="book-authors">Publisher: {this.props.publisher}</p> 
+        </div>
+        <button className="book-url"><a href={this.props.url}>See the Book</a></button>
       </div>
     )
   }
